c428842da481cd064150dc23467a444c777ec61a
endoxide <matefehervari24@gmail.com>
2024-09-11 00:56:17 +0100

    Add file exclusions to popup-nvim

--- Scripts/Scripts/popup-nvim.sh
+++ Scripts/Scripts/popup-nvim.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-file=$(fd --type f --hidden --exclude .git | fzf-tmux -p)
+file=$(fd --type f --hidden \
+  --exclude .git \
+  --exclude *.zip \
+  --exclude *.pdf \
+  --exclude *.png \
+  --exclude *.jpg \
+  --exclude *.bmp \
+  --exclude undodir \
+  --exclude site-packages \
+  --exclude .opam \
+  --exclude .pub-cache \
+  | fzf-tmux -p -w 90%)
 
 if [[ $file ]]; then
   echo $file | xargs nvim
