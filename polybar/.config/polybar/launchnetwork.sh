7e22e28315a2a66d97031c42c70fcbdd9927b2a1
endoxide <matefehervari24@gmail.com>
2024-09-18 07:46:47 +0100

    Fix polybar network toggle logging

--- polybar/.config/polybar/launchnetwork.sh
+++ polybar/.config/polybar/launchnetwork.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 killall -p networktoggle.sh
-$HOME/.config/polybar/networktoggle.sh 2>&1 | tee -a /tmp/network.log
+$HOME/.config/polybar/networktoggle.sh 2>&1 | tee -a /tmp/polybar/network.log
