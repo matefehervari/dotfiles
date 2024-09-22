7658bb57f12f27d2807a8f458c03eb8b6d09ec75
endoxide <matefehervari24@gmail.com>
2024-09-22 19:05:24 +0200

    Update killall in start barrier

--- Scripts/Scripts/i3/start-barrier.sh
+++ Scripts/Scripts/i3/start-barrier.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-killall -p barrierc
-sleep .5
-barrierc -f -n mLaptop --disable-crypto --restart $BARRIER_SERVER
-sleep .5
-barrierFixKB
+killall -o 5s barrierc
+barrierc -f -n mLaptop --disable-crypto --restart $BARRIER_SERVER && barrierFixKB
