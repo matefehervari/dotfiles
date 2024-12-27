623847fb5a20e391387ca853ca3ed7ccf6e9be75
Máté Fehérvári <matefehervari24@gmail.com>
2024-12-27 18:12:04 +0000

    Scripts update

--- Scripts/Scripts/i3/onedrive.sh
+++ Scripts/Scripts/i3/onedrive.sh
@@ -1,3 +1,3 @@
-rclone --vfs-cache-mode=full --vfs-read-ahead=10M --vfs-cache-max-age=1000h \
+rclone --copy-links --vfs-cache-mode=full --vfs-read-ahead=10M --vfs-cache-max-age=1000h \
        --vfs-cache-max-size=2G mount unionedrive: ~/OneDrive/unionedrive/
 # --umask 000
